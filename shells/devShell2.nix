06e3917f4ac396bd912d9adef0cbb94f1fb40187
cptfinch <cptfinch@gmail.com>
2024-05-20 15:24:59 +0100

    update

--- shells/devShell2.nix
+++ shells/devShell2.nix
@@ -1,25 +1,27 @@
 # shells/devShell2.nix
 { nixpkgs }:
 
-nixpkgs.legacyPackages.x86_64-linux.mkShell {
+let
+  pkgs = nixpkgs.legacyPackages.x86_64-linux;
+in pkgs.mkShell {
   buildInputs = [
-    nixpkgs.legacyPackages.x86_64-linux.ruby
-    nixpkgs.legacyPackages.x86_64-linux.rubyPackages.rails
-    nixpkgs.legacyPackages.x86_64-linux.rubyPackages.bundler
-    nixpkgs.legacyPackages.x86_64-linux.nodejs
-    nixpkgs.legacyPackages.x86_64-linux.yarn
-    nixpkgs.legacyPackages.x86_64-linux.postgresql
-    nixpkgs.legacyPackages.x86_64-linux.libpq
-    nixpkgs.legacyPackages.x86_64-linux.zlib
-    nixpkgs.legacyPackages.x86_64-linux.openssl
-    nixpkgs.legacyPackages.x86_64-linux.gnumake
-    nixpkgs.legacyPackages.x86_64-linux.gcc
-    nixpkgs.legacyPackages.x86_64-linux.gdbm
-    nixpkgs.legacyPackages.x86_64-linux.sqlite
+    pkgs.ruby
+    pkgs.rubyPackages.rails
+    pkgs.rubyPackages.bundler
+    pkgs.nodejs
+    pkgs.yarn
+    pkgs.postgresql
+    pkgs.libpq
+    pkgs.zlib
+    pkgs.openssl
+    pkgs.gnumake
+    pkgs.gcc
+    pkgs.gdbm
+    pkgs.sqlite
   ];
 
   shellHook = ''
-    export PATH=$PATH:${nixpkgs.legacyPackages.x86_64-linux.rubyPackages.bundler}/bin:${nixpkgs.legacyPackages.x86_64-linux.ruby}/bin
+    export PATH=$PATH:${pkgs.rubyPackages.bundler}/bin:${pkgs.ruby}/bin
     echo "Welcome to the Ruby on Rails development shell!"
   '';
 }
