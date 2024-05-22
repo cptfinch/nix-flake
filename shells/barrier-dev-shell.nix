14ae1d498fe5554d24b17d8cb5a48380f52fc27a
cptfinch <cptfinch@gmail.com>
2024-05-22 20:41:39 +0100

    Update

--- shells/barrier-dev-shell.nix
+++ shells/barrier-dev-shell.nix
@@ -4,13 +4,13 @@ nixpkgs.legacyPackages.x86_64-linux.mkShell {
   buildInputs = [
     nixpkgs.legacyPackages.x86_64-linux.cmake
     nixpkgs.legacyPackages.x86_64-linux.gcc
-    nixpkgs.legacyPackages.x86_64-linux.qt5
-    nixpkgs.legacyPackages.x86_64-linux.libX11
-    nixpkgs.legacyPackages.x86_64-linux.libXtst
-    nixpkgs.legacyPackages.x86_64-linux.libXi
-    nixpkgs.legacyPackages.x86_64-linux.openssl
-    nixpkgs.legacyPackages.x86_64-linux.curl
-    nixpkgs.legacyPackages.x86_64-linux.libXinerama
+    nixpkgs.legacyPackages.x86_64-linux.qt5.qtbase
+#    nixpkgs.legacyPackages.x86_64-linux.libX11
+#    nixpkgs.legacyPackages.x86_64-linux.libXtst
+#    nixpkgs.legacyPackages.x86_64-linux.libXi
+#    nixpkgs.legacyPackages.x86_64-linux.openssl
+#    nixpkgs.legacyPackages.x86_64-linux.curl
+#    nixpkgs.legacyPackages.x86_64-linux.libXinerama
   ];
 
   shellHook = ''
